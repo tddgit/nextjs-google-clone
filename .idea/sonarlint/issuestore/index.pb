dceecb60b3b6c9466841dbda83f38df252d8b313
Stas <holidayonmail@gmail.com>
2021-06-16 19:29:21 +0300

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+B
+tailwind.config.js,0/9/0918cbb76f2c44f223706cec9d31a32cd1177ce4
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+    return (
+        <div>
+    <Head>
+
+        </title>
+        Create Next App
+        </title>
+        <link rel="stylesheet" href="favicon.ico">
+    </Head>
+</div>;
+    )
+}
