756d6ad388d670dbb63583714cfdbad6620bd6c4
Stas <holidayonmail@gmail.com>
2021-06-17 19:29:41 +0300

    changed img on Image

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,11 +2,13 @@ import Image from 'next/image';
 
 function Avatar({ url, className }) {
     return (
-        <img
+        <Image
             src={url}
             alt="profile pic"
             loading="lazy"
             className={`h-10 transition duration-150 transform rounded-full cursor-pointer hover:scale-110 ${className}`}
+            height={40}
+            width={40}
         />
     );
 }
---
+++ dist/next.config.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['www.google.co.uk', 'coaching.papareact.com']
+  }
+};
